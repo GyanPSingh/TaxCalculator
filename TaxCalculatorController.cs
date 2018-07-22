9a4fa2a9d02d012c117da25381d1567d2db1993f
GyanPSingh <30523014+GyanPSingh@users.noreply.github.com>
2018-07-23 00:13:47 +0530

    Add files via upload
    Tax calculator controller

---
+++ TaxCalculatorController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using ACMEBusiness;
+using System.IO;
+namespace ACME_API.Controllers
+{
+    [Route("api/[controller]")]
+    public class TaxCalculatorController : Controller
+    {        
+        [HttpGet]
+        public IActionResult GeneratePaySlip()
+        {
+            try
+            {
+                PaySlipCalculator paySlipCalculator = new PaySlipCalculator();
+                string currentDirectory = Directory.GetCurrentDirectory();
+                string inputFilePath = System.IO.Path.Combine(currentDirectory, "InputFile", "Input.csv");
+                string outFilePath = System.IO.Path.Combine(currentDirectory, "OutputFile", "Output.csv");
+                paySlipCalculator.execute(inputFilePath, outFilePath);
+                return Ok("Pay slip has been generated.");
+            }
+            catch(Exception ex)
+            {
+                return NotFound(ex.Message);                 
+            }            
+        }       
+    }
+}
